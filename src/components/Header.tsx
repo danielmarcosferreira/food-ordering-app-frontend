241d38f320ad62f3949bfee2d7b7801cfd61a22c
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2024-10-11 12:04:30 -0300

    feat: add authenticated user links and improve mobile responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import MainNav from "./MainNav";
 
 const Header = () => {
     return (
-        <div className="border-b-2 border-b-orange-500 py-6">
+        <div className="border-b-2 border-b-orange-500 py-6 px-2">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-3x1 font-bold tracking-tight text-orange-500">
                     CityDelivery.com
