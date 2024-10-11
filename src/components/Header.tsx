7a611973d0643fd926a3008aafa3665a083e5ec1
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2024-10-10 21:38:58 -0300

    feat: implementation of landing page with responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
     return (
         <div className="border-b-2 border-b-orange-500 py-6">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-3x1 font-bold tracking-tight text-orange-500">
-                    BanaDelivery.com
+                    CityDelivery.com
                 </Link>
+                <div className="md:hidden">
+                    <MobileNav />
+                </div>
+                <div className="hidden md:block">
+                    <MainNav />
+                </div>
             </div>
         </div>
     )
